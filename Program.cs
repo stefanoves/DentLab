8d8daf4d00c57835cac0ec95c4807226803eefac
stefanoves <estefanov@students.softuni.bg>
2024-04-21 22:28:48 +0300

    Readme actualization
    The purpose of the project has been clarified. It is intended to replace the use of Google's Keep or any other note-taking app due to their lack of order management features and storage space

--- Program.cs
+++ Program.cs
@@ -4,19 +4,14 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
@@ -24,7 +19,6 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # DentLab
+#
+# Order Management in a Dental Laboratory.
+# This is a simple order management system for a dental laboratory. 
+# The main purpose should be replasing the use of Gooogle's Keep or any other note taking app, due to lack of order management features and storage space.
